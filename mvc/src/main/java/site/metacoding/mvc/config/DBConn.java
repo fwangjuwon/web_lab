e51f1e886297e42374a3aef723d5d2fc16016644
klkjjj4848 <fwangjuwon@naver.com>
2022-02-24 12:47:41 +0900

    mvc패턴 구조 설정 완료

---
+++ mvc/src/main/java/site/metacoding/mvc/config/DBConn.java
@@ -0,0 +1,27 @@
+package site.metacoding.mvc.config;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConn {
+
+	//책임: 이미 만들어진 connection객체를 return시킴
+	public static Connection getConnection() {
+Connection conn = null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+			System.out.println("db연결성공");
+			//etc.
+		} catch (Exception e) {
+			System.out.println("db연결실패");
+			e.printStackTrace();
+		}
+return conn;
+	}
+}
